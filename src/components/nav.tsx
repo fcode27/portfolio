3e778db3032afc55c12ef0416851e5524dccb1d5
fcode27 <7506446+fcode27@users.noreply.github.com>
2025-06-01 16:57:37 -0400

    update theme switcher

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -24,26 +24,26 @@ export default function Nav() {
   ];
 
   return (
-    <div className="fixed top-5 left-0 z-50 w-full">
+    <div className="fixed top-5 left-0 z-50 w-full ">
+      <div className="ml-8 top-2 right-10 absolute flex items-center justify-end gap-2">
+        <ThemeSwitcher />
+      </div>
       <nav className="text-main-foreground border-border shadow-shadow rounded-base bg-main font-base w450:gap-4 mx-auto flex w-max border-2 p-1.5 pl-2 pr-3 text-sm sm:text-base">
         <div className="flex gap-5">
           {links.map((link) => {
-        return (
-          <Link
-            key={link.path}
-            className={clsx(
-          "hover:border-border rounded-base border-2 px-2 py-2 transition-colors",
-          path === link.path ? "border-border" : "border-transparent",
-            )}
-            href={link.path}
-          >
-            {link.text}
-          </Link>
-        );
+            return (
+              <Link
+                key={link.path}
+                className={clsx(
+                  "hover:border-border rounded-base border-2 px-2 py-2 transition-colors",
+                  path === link.path ? "border-border" : "border-transparent",
+                )}
+                href={link.path}
+              >
+                {link.text}
+              </Link>
+            );
           })}
-        </div>
-        <div className="ml-8">
-          <ThemeSwitcher />
         </div>
       </nav>
     </div>
--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,19 +1,31 @@
-'use client'
+"use client";
 
-import { Moon, Sun } from 'lucide-react'
-import { useTheme } from 'next-themes'
+import {
+  Moon,
+  Sun,
+  MoonIcon,
+  MoonStar,
+  MoonStarIcon,
+  SunMoon,
+} from "lucide-react";
+import { useTheme } from "next-themes";
 
-import * as React from 'react'
-import { Button } from './ui/button'
+import * as React from "react";
+import { Button } from "./ui/button";
 
 export function ThemeSwitcher() {
-  const { setTheme, theme } = useTheme()
+  const { setTheme, theme } = useTheme();
 
   return (
     <>
-      <Button size={"sm"} onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-        <Sun className="stroke-main-foreground hidden size-4 sm:size-6 dark:inline" />
-        <Moon className="stroke-main-foreground inline size-4 sm:size-6 dark:hidden" />
+      <Button
+        className="bg-trasparent"
+        color="--var-primary"
+        size={"sm"}
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      >
+        <Sun strokeWidth={3} className="stroke-white  hidden dark:inline" />
+        <Moon strokeWidth={3} className="stroke-black inline dark:hidden" />
         <span className="sr-only">Toggle theme</span>
       </Button>
     </>
