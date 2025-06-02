cd38bf915881f18a0c67b586345bf8768c4e0644
fcode27 <7506446+fcode27@users.noreply.github.com>
2025-06-02 15:07:09 -0400

    add project data and images for portfolio showcase

---
+++ src/data/projects.ts
@@ -0,0 +1,74 @@
+const PROJECTS: {
+  name: string;
+  description: string;
+  previewImages: string[];
+  repoUrl: string;
+  liveLink: string;
+  side: "top" | "bottom" | "left" | "right" | undefined;
+}[] = [
+  {
+    name: "Portfolio",
+    description:
+      "A personal portfolio project built with Next.js and TypeScript, designed to showcase a collection of software development projects. This responsive and dynamic website features a clean design and easy navigation, highlighting work with React, mobile applications, and backend technologies. Visitors can explore various projects and find contact information for inquiries.",
+    liveLink: "",
+    previewImages: ["website1.png"],
+    repoUrl: "",
+    side: "left",
+  },
+  {
+    name: "Dining Decision Manager",
+    description:
+      "An app that helps users manage their dining decisions and find the perfect restaurant and best dishes worldwide. Built 50% of all the release versions using React Native and previously Ionic Framework before it's migration. Focused on building the UI and logic for all the tabs and sections of the app.",
+    liveLink: "",
+    previewImages: ["dining1.png", "dining2.png"],
+    repoUrl: "",
+    side: "left",
+  },
+  {
+    name: "Festival Navigator",
+    description:
+      "An app that helps users find their way around the festival. Participants can browse through panel discussion topics and find the speakers they want to hear. Ability to receive real-time event updates and plan the day with a customizable calendar. It's available in the US App Store and Google Play. Built 50% of the first release version using React Native.",
+    liveLink: "",
+    previewImages: ["festival_sign.png", "festival_agenda.png"],
+    repoUrl: "",
+    side: "left",
+  },
+  {
+    name: "Car Community Coordinator",
+    description:
+      "Android application which helps to coordinate car meets, find auto repair shops, car groups and more. Focused on the user following client side logic using React Native and Redux. The application is still in development.",
+    liveLink: "",
+    previewImages: ["car-community2.png", "car-community.png"],
+    repoUrl: "",
+    side: "left",
+  },
+  {
+    name: "QuickShop Fashion App",
+    description:
+      "An app that allows users to purchase clothing, shoes, jewelry, and more directly and securely in just a few taps. You can sift through their special collections, add to favorites or check out the items you picked with delivery service to your door or get to the store later for pickup. Built all the UI of the favorites section, shipping method, pick up address selection and delivery address form.",
+    liveLink: "",
+    previewImages: ["quickshop1.png", "quickshop2.png"],
+    repoUrl: "",
+    side: "left",
+  },
+  {
+    name: "Transport Finance Solutions",
+    description:
+      "A transportation-focused factoring company specializing in providing reliable cash flow solutions for the industry.",
+    liveLink: "",
+    previewImages: ["transport-finance1.png"],
+    repoUrl: "",
+    side: "left",
+  },
+  {
+    name: "Truck Driver Connect",
+    description:
+      "A mobile application empowering truck drivers to communicate with customers, enabling load tracking, document management, signature collection, and more.",
+    liveLink: "",
+    previewImages: ["truck-drivers1.png", "truck-drivers2.png"],
+    repoUrl: "",
+    side: "left",
+  },
+];
+
+export default PROJECTS;
