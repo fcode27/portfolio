9c6347a2dfbebd4a274f9e4ec78d569713e359c8
fcode27 <7506446+fcode27@users.noreply.github.com>
2025-06-02 15:25:22 -0400

    add experience to the about section

---
+++ src/data/experience.ts
@@ -0,0 +1,68 @@
+const PAST_ROLES: {
+  company: string;
+  role: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+}[] = [
+  {
+    company: "Develappers GmbH",
+    role: "Mobile Application Developer (Android & iOS)",
+    description:"",
+    startDate: "May 2022",
+    endDate: "February 2024",
+  },
+  {
+    company: "Lean Tech",
+    role: "Project Lead Developer",
+    description:"",
+    startDate: "Nov 2020",
+    endDate: "Apr 2022",
+  },
+
+  {
+    company: "The World Bank",
+    role: "Software Development Consultant",
+    description:"",
+
+    startDate: "Jun 2020",
+    endDate: "Apr 2022",
+  },
+  {
+    company: "RECYDUO App",
+    role: "Senior Software Engineer",
+    description:"",
+    startDate: "Sept 2020",
+    endDate: "April 2022",
+  },
+  {
+    company: "TidBit Social",
+    role: "Frontend Developer",
+    description:"",
+    startDate: "June 2018",
+    endDate: "April 2021",
+  },
+  {
+    company: "Turpial Development",
+    role: "Software Developer (Web & Mobile)",
+    description:"",
+    startDate: "Jun 2016",
+    endDate: "May 2017",
+  },
+  {
+    company: "Freelance",
+    role: "Software Developer",
+    description:"",
+    startDate: "Sept 2016",
+    endDate: "Now",
+  },
+  {
+    role: "IT Marketing Intern",
+    company: "British American Tobacco",
+    description:"",
+    startDate: "Jul 2015",
+    endDate: "Oct 2015",
+  },
+];
+
+export default PAST_ROLES;
